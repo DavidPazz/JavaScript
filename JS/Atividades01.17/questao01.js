9c94d5ee5de715c806525f815ca1439d8c148ba2
David Paz <davizinpaz007@gmail.com>
2024-01-19 19:08:33 -0300

    .

--- JS/Atividades01.17/questao01.js
+++ JS/Atividades01.17/questao01.js
@@ -2,8 +2,55 @@
  */
 
 
-const prompt = require('prompt-sync') ()
+// Não entendi a parte na parte por Extenso
+
+
+/*const prompt = require('prompt-sync') ()
 let data = prompt('digite a data desejada. EX:DD/MM/AAAA: ')
 let arraydata = data.split('/')
 
-console.log(data)
+console.log(data) */
+
+function data(DD, MM, AAAA) {
+    if (DD > 31) {
+        return null
+    } else if (DD < 0){
+        return null
+    }
+    if (MM > 12) {
+        return null
+    } else if (MM < 0) {
+        return null
+    }
+    
+    AAAA = AAAA
+
+    switch (MM) {
+        case 1:
+            return ${DD} de Janeiro de ${AAAA}
+        case 2:
+            return ${DD} de Fevereiro de ${AAAA}
+        case 3:
+            return ${DD} de Março de ${AAAA}
+        case 4:
+            return ${DD} de Abril de ${AAAA}
+        case 5:
+            return ${DD} de Maio de ${AAAA}
+        case 6:
+            return ${DD} de Junho de ${AAAA}
+        case 7:
+            return ${DD} de Julho de ${AAAA}
+        case 8:
+            return ${DD} de Agosto de ${AAAA}
+        case 9:
+            return ${DD} de Setembro de ${AAAA}
+        case 10:
+            return ${DD} de Outubro de ${AAAA}
+        case 11:
+            return ${DD} de Novembro de ${AAAA}
+        case 12:
+            return ${DD} de Dezembro de ${AAAA}
+    }
+}
+
+console.log(data(22, 5, 1999));
